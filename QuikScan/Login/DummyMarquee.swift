c0732ebf3abdb636076225b9371695c886739597
Kailash Sharma <kailashps.1011@gmail.com>
2024-03-28 04:34:08 +0530

    Initial

---
+++ QuikScan/Login/DummyMarquee.swift
@@ -0,0 +1,18 @@
+//
+//  DummyMarquee.swift
+//  QuikScan
+//
+//  Created by Kailash on 28/03/24.
+//
+
+import SwiftUI
+
+struct DummyMarquee: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DummyMarquee()
+}
---
+++ QuikScan/Utils/AllFonts.swift
@@ -0,0 +1,18 @@
+//
+//  AllFonts.swift
+//  QuikScan
+//
+//  Created by Kailash on 28/03/24.
+//
+
+import SwiftUI
+
+struct AllFonts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AllFonts()
+}
