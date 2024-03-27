10b07a22f8e94d7d63ee714dade382699df92914
Kailash Sharma <kailashps.1011@gmail.com>
2024-03-27 22:35:38 +0530

    Initial Commit

---
+++ QuikScan/QuikScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuikScanApp.swift
+//  QuikScan
+//
+//  Created by Kailash on 27/03/24.
+//
+
+import SwiftUI
+
+@main
+struct QuikScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
