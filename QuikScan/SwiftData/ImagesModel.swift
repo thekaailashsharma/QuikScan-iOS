53ec07b64bed91f917e488beeef833c0c9e3ab2d
Kailash Sharma <kailashps.1011@gmail.com>
2024-04-02 03:45:24 +0530

    Add Search Logic & Grouped Lists

---
+++ QuikScan/SwiftData/ImagesModel.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewModel.swift
+//  QuikScan
+//
+//  Created by Kailash on 01/04/24.
+//
+
+import SwiftUI
+
+enum Images: String {
+    case urlNone = "url_none"
+    case urlLinkedIn = "url_linkedIN"
+    case urlMedium = "url_medium"
+    case urlTwitter = "url_twitter"
+    case urlGithub = "url_github"
+    case urlFacebook = "url_facebook"
+    case urlWhatsapp = "url_whatsapp"
+    case urlInstagram = "url_instagram"
+    case barMail = "bar_mail"
+    case barSMS = "bar_sms"
+    case barPhone = "bar_phone"
+    case barvCard = "bar_vcard"
+    case barText = "bar_text"
+    case barCode = "barcode"
+}
